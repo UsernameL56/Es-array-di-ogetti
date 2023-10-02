4659baad0650320318b57e8026d44e97dc370110
Lucas Corsi <corsi.lucasgabriel.studente@itispaleocapa.it>
2023-10-02 23:22:13 +0200

    Aggiungere i file di progetto.

---
+++ Es array di ogetti/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using classe;
+
+namespace Es_array_di_ogetti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserire il numero di alunni che si vogliono inserire: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Studenti[] studenti = new Studenti[n];
+
+            for(int i = 0; i < n; i++) 
+            {
+                Console.Clear();
+                Console.WriteLine("Inserire il nome dello studente: ");
+                string nome = Console.ReadLine();
+
+                Console.WriteLine("Inserire l'età dello studente: ");
+                int eta = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Inserire il votgo dello studente: ");
+                int voto = int.Parse(Console.ReadLine());
+
+                studenti[i] = new Studenti(nome, eta, voto);
+            }
+
+            Console.Clear();
+            Console.WriteLine($"Elenco di tutti gli alunni: {studenti[0].Elenco(studenti)}");
+            Console.WriteLine($"Media dei voti di tutti gli alunni: {studenti[0].Media(studenti)}");
+        }
+    }
+}
---
+++ classe/Class1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classe
+{
+    public class Class1
+    {
+    }
+
+    public class Studenti
+    {
+        // attributi
+        private string nome;
+        private int età;
+        private int voto;
+
+        // costruttore senza parametri
+        public Studenti() 
+        {
+            nome = "";
+            età = 0;
+            voto = 0;
+        }
+
+        // costruttore con parametri
+        public Studenti(string nome, int età, int voto)
+        {
+            Name = nome;
+            Age = età;
+            Grade = voto;
+        }
+
+        // metodi accessor
+        public string Name 
+        { 
+            get { return nome; } 
+            set { nome = value; }
+        }
+        public int Age
+        {
+            get { return età; }
+            set { età = value; }
+        }
+        public int Grade
+        {
+            get { return voto; }
+            set { voto = value; }
+        }
+
+        // metodi
+        public double Media(Studenti[] array)
+        {
+            double temp = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                temp += array[i].Grade;
+            }
+            return (temp/array.Length);
+        }
+
+        public string Elenco(Studenti[] array)
+        {
+            string temp = "";
+            for (int i = 0; i < array.Length; i++)
+            {
+                temp += array[i].Name + "; ";
+            }
+            return temp;
+        }
+    }
+}
